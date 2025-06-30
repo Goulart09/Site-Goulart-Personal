4b9117e196f95497f3fe027d2ccef9e4ce9d9858
Goulart09 <guimelgoulart09@gmail.com>
2025-06-30 10:08:25 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-// Lógica JS para login, navegação e Firebase
+
+function login() {
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+  const error = document.getElementById('error');
+  if (password === 'treino123') {
+    window.location.href = 'dashboard.html';
+  } else {
+    error.textContent = 'Senha incorreta. Use: treino123';
+  }
+}
