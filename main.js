3b9acd63980fa82bfd674a8b970bc3124eb2664b
Goulart09 <guimelgoulart09@gmail.com>
2025-07-01 07:09:23 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,9 @@
-document.getElementById('loginForm').addEventListener('submit', function(e) {
-  e.preventDefault();
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
-  const errorMsg = document.getElementById('error-msg');
-
-  if (email === 'caiovictorarantes@gmail.com' && password === 'Caio') {
-    window.location.href = 'dashboard.html';
+function login() {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  if (email === "caiovictorarantes@gmail.com" && password === "Caio") {
+    window.location.href = "dashboard.html";
   } else {
-    errorMsg.textContent = 'Senha incorreta';
+    document.getElementById("error-message").innerText = "Senha incorreta";
   }
-});
+}
