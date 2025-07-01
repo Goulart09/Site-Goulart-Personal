4256c3e3c5862bd09d998f3208c4753beac87d1e
Goulart09 <guimelgoulart09@gmail.com>
2025-07-01 07:56:23 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-document.getElementById("login-form").addEventListener("submit", function(e) {
-  e.preventDefault();
-  alert("Login funcional com Firebase será implementado aqui.");
-});
+
+function login(event) {
+    event.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const errorMessage = document.getElementById('error-message');
+
+    if (email === "caiovictorarantes@gmail.com" && password === "Caio") {
+        window.location.href = "dashboard.html";
+    } else {
+        errorMessage.textContent = "Senha incorreta";
+    }
+}
